a8dfcd21bddb87df44cc87e24db606828f81080e
shauryasds <119954061+shauryasds@users.noreply.github.com>
2024-01-21 22:27:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ async function apiFetch(){
     
     let city=document.getElementById("city").value;
     
-    document.getElementByClassName("city")[0].innerText='SEARCHING';
+    document.getElementsByClassName("city")[0].innerText='SEARCHING';
+    document.getElementsByClassName("temp")[0].innerText= '';
+    document.getElementsByClassName("humidity")[0].innerText= "--";
+
+    document.getElementsByClassName("windspeed")[0].innerText= "--";
+    
 const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     let  Apiresponse=await fetch(url);
@@ -19,7 +24,7 @@ const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api
 
     document.getElementsByClassName("humidity")[0].innerText= response.main.humidity;
 
-    document.getElementsByClassName("windspeed")[0].innerText= response.wind.speed;
+    document.getElementsByClassName("windspeed")[0].innerText= response.wind.speed +"km/h";
     }
     catch{
         response='Invalid City'
@@ -38,7 +43,6 @@ const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api
 
 
 }
-document.getElementByClassName("city")[0].innerText='WEATHER APP';
 search.addEventListener('click',(e)=>{
     apiFetch();
     
